3a2c49bc3272cd8e400493e2eb8270679b6243f0
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2024-01-29 12:33:01 +1300

    add moving pipe

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,18 @@ struct FLAPPER {
 
 const MOOMOO_HEIGHT: u32 = 30;
 const MOOMOO_WIDTH: u32 = 30;
+const FALL_SPEED: u32 = 3;
+const BOUNCE_SPEED: u32 = 5;
+
+struct PIPE {
+    x_pos: u32,
+    upper_x: u32,
+    center: u8,
+    width: u32,
+}
+
+const PIPE_SPEED: u32 = 3;
+const PIPE_WIDTH: u32 = 50;
 
 fn main() {
     let apt = Apt::new().unwrap();
@@ -21,7 +33,13 @@ fn main() {
         upper_y: 180,
         falling: true,
     };
-    let mut jump_prog: u8 = 0;
+    let mut pipe1 = PIPE {
+        x_pos: 300,
+        upper_x: 300 + PIPE_WIDTH,
+        center: 120,
+        width: PIPE_WIDTH,
+    };
+    let mut bounce_prog: u8 = 0;
     while apt.main_loop() {
         hid.scan_input();
         if hid.keys_down().contains(KeyPad::START) {
@@ -29,16 +47,17 @@ fn main() {
         }
         if hid.keys_down().contains(KeyPad::A) {
             moomoo.falling = false;
-            jump_prog = 0;
+            bounce_prog = 0;
         }
         if !moomoo.falling {
-            jump_prog += 1;
-            if jump_prog >= 15 {
+            bounce_prog += 1;
+            if bounce_prog >= 12 {
                 moomoo.falling = true;
-                jump_prog = 0;
+                bounce_prog = 0;
             }
         }
         movement(&mut moomoo);
+        pipe_movement(&mut pipe1);
         {
             let top_screen = gfx.top_screen.get_mut();
             let frame_buffer = top_screen.raw_framebuffer();
@@ -50,6 +69,15 @@ fn main() {
                 MOOMOO_HEIGHT,
                 SQUARE_COLOR,
                 true,
+            );
+            draw_filled_square(
+                &frame_buffer,
+                pipe1.x_pos,
+                0,
+                pipe1.width,
+                240,
+                PIPECOLOR,
+                false,
             );
             top_screen.flush_buffers();
             top_screen.swap_buffers();
@@ -63,7 +91,8 @@ const SQUARE_COLOR_G: u8 = 0;
 const SQUARE_COLOR_B: u8 = 0;
 
 static SQUARE_COLOR: [u8; 3] = [SQUARE_COLOR_B, SQUARE_COLOR_G, SQUARE_COLOR_R];
-static BLANK: [u8; 3] = [0, 0, 0];
+static BLANK: [u8; 3] = [255, 208, 34];
+static PIPECOLOR: [u8; 3] = [63, 181, 45];
 
 fn draw_filled_square(
     frame_buffer: &RawFrameBuffer<'_>,
@@ -97,10 +126,38 @@ fn draw_filled_square(
 
 fn movement(moomoo: &mut FLAPPER) {
     if moomoo.falling && moomoo.y_pos > 5 {
-        moomoo.y_pos -= 2;
-        moomoo.upper_y -= 2;
+        moomoo.y_pos -= FALL_SPEED;
+        moomoo.upper_y -= FALL_SPEED;
     } else if moomoo.upper_y < 235 && !moomoo.falling {
-        moomoo.y_pos += 4;
-        moomoo.upper_y += 4;
+        moomoo.y_pos += BOUNCE_SPEED;
+        moomoo.upper_y += BOUNCE_SPEED;
+    }
+}
+
+fn pipe_movement(pipe: &mut PIPE) {
+    if pipe.width == 0 {
+        pipe.x_pos = 400;
+    }
+    if pipe.x_pos >= PIPE_SPEED && pipe.x_pos <= 400 - PIPE_WIDTH {
+        pipe.x_pos -= PIPE_SPEED;
+        pipe.upper_x -= PIPE_SPEED;
+    } else if pipe.x_pos <= 400 - PIPE_WIDTH {
+        pipe.x_pos = 0;
+        if pipe.width >= PIPE_SPEED {
+            pipe.width -= PIPE_SPEED;
+            pipe.upper_x -= PIPE_SPEED;
+        } else {
+            pipe.width = 0;
+            pipe.upper_x = 0;
+        }
+    } else {
+        pipe.upper_x = 400;
+        pipe.x_pos -= 3;
+        pipe.width += 3;
+        if pipe.width >= PIPE_WIDTH {
+            pipe.width = PIPE_WIDTH;
+            pipe.x_pos = 400 - PIPE_WIDTH;
+            pipe.upper_x = pipe.x_pos + PIPE_WIDTH;
+        }
     }
 }
